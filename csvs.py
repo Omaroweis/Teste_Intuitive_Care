89fdf731a3df0d2eb6119f215e285c534f6e3355
Omar <omararaujolopes1@gmail.com>
2021-05-13 16:28:46 -0300

    Update csvs.py

--- csvs.py
+++ csvs.py
@@ -5,12 +5,12 @@ def makeCsvs():
     pdf = convert_into("arquivo.pdf", "arquivo.tsv", pages=80, output_format="tsv")
     df = pd.read_csv("arquivo.tsv", sep="\t")
     arquivos = []
-    for i in df.values:
-        for j in i:
-            if str(j) == "30" or str(j) == "31" or str(j) == "32":
-                aux = "Informacao da linha " + str(j) + ".csv";
+    for linha in df.values:
+        for palavra in linha:
+            if str(palavra) == "30" or str(palavra) == "31" or str(palavra) == "32":
+                aux = "Informacao da linha " + str(palavra) + ".csv";
                 file = open(aux, "w");
-                file.write(str(i))
+                file.write(str(linha))
                 arquivos.append(aux)
 
     return arquivos;
